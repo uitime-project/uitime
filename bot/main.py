a96ed621a1e48d55b731d9cf2322ccfaec28f756
“Vladyslav <vlad.korniychuk12@gmail.com>
2026-04-25 17:30:24 +0200

    feat: wire up main.py

--- bot/main.py
+++ bot/main.py
@@ -2,27 +2,31 @@ import asyncio
 import logging
 import sys
 import os
+from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
+
 from handlers import auth_router
+from scheduler import setup_scheduler 
 
 load_dotenv()
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def main():
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-
+    
     if not BOT_TOKEN:
-        logging.error("CRITICAL: BOT_TOKEN enviroment variable is misiing")
+        logging.error("CRITICAL: BOT_TOKEN environment variable is missing!")
         sys.exit(1)
 
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
-
+    
     dp.include_router(auth_router)
-
-    logging.info("Starting bot pooling...")
+    
+    setup_scheduler(bot)
+    
+    logging.info("Starting bot polling...")
     try:
         await dp.start_polling(bot)
     finally:
